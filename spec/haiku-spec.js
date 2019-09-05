0f6174ecb48cbc065def26077c617414d35b693c
Student <example@example.com>
2019-09-05 15:48:14 -0700

    add karma tests

--- spec/haiku-spec.js
+++ spec/haiku-spec.js
@@ -1,4 +1,4 @@
-import { getSyllables } from './../src/main.js';
+import { getSyllables } from './../src/syllables.js';
 
 describe("getSyllables", function() {
   it("will ignore non-alphabetical characters", function() {
@@ -10,7 +10,19 @@ describe("getSyllables", function() {
   it("will output 1 if the word length is 1", function() {
     expect(getSyllables("I")).toEqual(1);
   });
-  // it("will ....", function() {
-  //   expect(getSyllables("book")).toEqual(1);
-  // });
+  it("will correctly identify single syllable words", function() {
+    expect(getSyllables("book")).toEqual(1);
+  });
+  it("will correctly identify multiple syllable words", function() {
+    expect(getSyllables("javascript")).toEqual(3);
+  });
+  it("will correctly words with no vowels except y", function() {
+    expect(getSyllables("rhythym")).toEqual(2);
+  });
+  it("will correctly identify words with multiple leading vowels", function() {
+    expect(getSyllables("aioli")).toEqual(3);
+  });
+  it("will correctly identify words with lots of syllables", function() {
+    expect(getSyllables("pseudopseudohypoparathyroidism")).toEqual(11);
+  });
 });
